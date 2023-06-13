382cae4bcedfc56a829cd1c58915d459a69dc4f1
Darkhackit <earthur510@gmail.com>
2023-06-13 17:07:59 +0000

    Update to prime extension

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,7 @@
 let notification_id = false
+let response : { [p: string]: any } = {}
 const getUpdatedNotification = (notification:string) => {
+    console.log({notification:'updated'})
     setTimeout(() => {
         chrome.notifications.getAll(permission => {
             const notifications = Object.keys(permission)
@@ -18,12 +20,6 @@ const cancelNotification = () => {
             chrome.notifications.clear(notification)
         })
     })
-    // if(notification_id) {
-    //     notification_id = false
-    //     chrome.alarms.create({ delayInMinutes: 5 })
-    // }else {
-    //     notification_id = true
-    // }
 }
 const updateNotification = () => {
     chrome.notifications.create({
@@ -52,6 +48,7 @@ const recallNotification = () => {
         },getCurrentNotification)
 }
 const getCurrentNotification = (notification:string) => {
+    console.log({notification:'add'})
     setTimeout(() => {
         chrome.notifications.getAll(permission => {
             const notifications = Object.keys(permission)
@@ -63,61 +60,92 @@ const getCurrentNotification = (notification:string) => {
     },60000)
 };
 //Start Notification
-chrome.runtime.onStartup.addListener(() => {
+chrome.runtime.onStartup.addListener(async () => {
     chrome.alarms.getAll(alarms => {
         console.log(alarms)
         if(!alarms.length) {
-            notification_id = false
-            chrome.alarms.create({ delayInMinutes: 1 })
-        }
+            chrome.alarms.create({delayInMinutes: 60})
+         }
     })
 })
 
 //On Alarm
-chrome.alarms.onAlarm.addListener(() => {
-    chrome.notifications.create("start_notification",{
-        type: 'basic',
-        iconUrl: 'assets/stay_hydrated.png',
-        title: 'Kindly stretch your body or take a walk',
-        message: "Kindly stretch your body or take a walk",
-        buttons: [{ title: 'I have started stretching' }],
-        requireInteraction:true,
-        silent:false,
-        priority: 0
-    },getCurrentNotification)
+chrome.alarms.onAlarm.addListener(async () => {
+    response = await chrome.storage.sync.get(['type'])
+    if(response.type === 'add') {
+        chrome.notifications.create('add',{
+            type: 'basic',
+            iconUrl: 'assets/stay_hydrated.png',
+            title: 'Kindly stretch your body or take a walk',
+            message: "Kindly stretch your body or take a walk",
+            buttons: [{ title: 'I have started stretching' }],
+            requireInteraction:true,
+            silent:false,
+            priority: 0
+        },getCurrentNotification)
+    }
+    if (response.type === 'update') {
+            chrome.notifications.create('update',{
+                type: 'basic',
+                iconUrl: 'assets/stay_hydrated.png',
+                title: 'Time up please',
+                message: "Time up please",
+                buttons: [{ title: 'Am seated' }],
+                requireInteraction:true,
+                silent:false,
+                priority: 0
+            },getUpdatedNotification)
+    }
 
 });
 
 chrome.notifications.onButtonClicked.addListener(async () => {
     cancelNotification()
-    chrome.alarms.create({ delayInMinutes: 1 })
-    // setTimeout(() => {
-    //     if (notification_id) {
-    //         updateNotification()
-    //     }
-    //
-    // },60000)
+    if (response.type === 'add') {
+        await chrome.storage.sync.set({type:'update'})
+        await chrome.alarms.create({ delayInMinutes: 10 })
+
+    }
+
+    if (response.type === 'update') {
+        await chrome.storage.sync.set({type:'add'})
+        await chrome.alarms.create({ delayInMinutes: 60 })
+    }
 });
-chrome.idle.onStateChanged.addListener(listener => {
+chrome.idle.onStateChanged.addListener(async (listener) => {
     console.log(listener)
     if(listener === 'locked') {
-        chrome.alarms.clearAll();
+     await chrome.alarms.clearAll()
         chrome.notifications.getAll(permission => {
             const notifications = Object.keys(permission)
             notifications.map(notification => {
                 chrome.notifications.clear(notification)
             })
         })
-
     }
     if(listener === 'active') {
         chrome.alarms.getAll(alarms => {
             console.log(alarms)
-            if(!alarms.length && !notification_id) {
-                chrome.alarms.create({ delayInMinutes: 1 })
+            if(!alarms.length) {
+                chrome.alarms.create({ delayInMinutes: 60 })
             }
         })
 
     }
+
+
+    // await chrome.storage.sync.set({type:'add'})
+    // response = await chrome.storage.sync.get(['type'])
+    // console.log(await response.type)
+    // if(response.type == 'add') {
+    //     console.log('Hey')
+    //    await  chrome.storage.sync.set({type:'update'})
+    //     response = await chrome.storage.sync.get(['type'])
+    // }
+    // console.log(response)
+})
+
+chrome.storage.onChanged.addListener(async (changes) => {
+    console.log(changes)
 })
 
